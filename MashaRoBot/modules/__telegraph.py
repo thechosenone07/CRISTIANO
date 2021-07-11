944e61150e994e0fd1e84f65f805b9094d1ad503
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 15:08:15 +0530

    Update __telegraph.py

--- MashaRoBot/modules/__telegraph.py
+++ MashaRoBot/modules/__telegraph.py
@@ -80,9 +80,10 @@ def resize_image(image):
 
 
 __help__ = """
-I can upload files to Telegraph
- ❍ /tm :Get Telegraph Link Of Replied Media
- ❍ /txt :Get Telegraph Link of Replied Text
+Uploads files to Telegraph.
+
+• `/tm`*:*Get Telegraph Link Of Replied Media
+• `/txt` *:*Get Telegraph Link of Replied Text
 """
 
 __mod_name__ = "T GRAPH"
