67158e9ac5b892fffa14c39dae7acc09f247171b
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 14:52:53 +0530

    Update tools.py

--- MashaRoBot/modules/tools.py
+++ MashaRoBot/modules/tools.py
@@ -2,26 +2,16 @@ __mod_name__ = "Tools"
 
 __help__ = """
 • `/country` `<country name>`*:* Gathering info about given *country*
-
 • `/cs`*:* Latest live scores from cricinfo
-
 • `/gps` `<location>`*:* Get gps location.
  Example : `/gps India`
-
 • `/imdb` `<movie/seires name>`*:* Get full info about a movie & series with [imdb.com](https://m.imdb.com)
-
-*Zipper* :
 • `/zip`*:* reply to a telegram file to compress it in .zip format
 • `/unzip`*:* reply to a telegram file to decompress it from the .zip format
-
-*Weather* :
 • `/weather` `<city>`*:* Get weather info in a particular place.
 • `/wttr` `<city>`*:* Advanced weather module, usage same as /weather
  Eg. `/wttr moon*:* Get the current status of moon
-
-*Secure* :
 • `/encrypt`*:* Encrypts The Given Text
 • `/decrypt`*:* Decrypts Previously Ecrypted Text
-
 • `/json`*:* Get JSON info about any message replied to.
 """
