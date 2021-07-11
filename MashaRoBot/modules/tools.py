ad61d686ea1f4b80c367f7935fe852dad1c34951
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 14:55:54 +0530

    Update tools.py

--- MashaRoBot/modules/tools.py
+++ MashaRoBot/modules/tools.py
@@ -1,6 +1,7 @@
 __mod_name__ = "Tools"
 
 __help__ = """
+
 • `/country` `<country name>`*:* Gathering info about given *country*
 • `/cs`*:* Latest live scores from cricinfo
 • `/gps` `<location>`*:* Get gps location.
