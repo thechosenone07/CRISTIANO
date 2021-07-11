55c1e9f6c2072e154c93320a57fb7573aa6938e2
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-07-11 12:45:58 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
-
+from uuid import uuid4
+from math import ceil
 from MashaRoBot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
