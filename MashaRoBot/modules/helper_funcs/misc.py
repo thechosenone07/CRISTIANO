6c5519158c33f0f103d5407ae1da3bdcffc48636
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 13:40:47 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -80,7 +80,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⏮️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Home", callback_data="masha_back"),
+                EqInlineKeyboardButton("🏠 Home", callback_data="masha_back"),
              EqInlineKeyboardButton("⏭️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
 
