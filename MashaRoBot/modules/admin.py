93b76633eb3f51c4551a5d422f618e13c0b81241
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 13:25:46 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -628,22 +628,21 @@ def adminlist(update, context):
 
 
 __help__ = """
- ❍ `/admins`*:* list of admins in the chat
+• `/admins`*:* list of admins in the chat
 
 *Admins only:*
- ❍ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
- ❍ `/unpin`*:* unpins the currently pinned message
- ❍ `/invitelink`*:* gets invitelink
- ❍ `/promote`*:* promotes the user
- ❍ `/demote`*:* demotes the user
- ❍ `/title` `<admin title>`*:* sets a custom title for an admin that the bot promoted
- ❍ `/admincache`*:* force refresh the admins list
- ❍ `/antispam` `<on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
- ❍ `/addnt`*:* Adds Group to NightMode Chats
- ❍ `/rmnt`*:* Removes Group From NightMode Chats
+• `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+• `/unpin`*:* unpins the currently pinned message
+• `/invitelink`*:* gets invitelink
+• `/promote`*:* promotes the user
+• `/demote`*:* demotes the user
+• `/title` `<admin title>`*:* sets a custom title for an admin that the bot promoted
+• `/admincache`*:* force refresh the admins list
+• `/antispam` `<on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+• `/addnt`*:* Adds Group to NightMode Chats
+• `/rmnt`*:* Removes Group From NightMode Chats
 
-*Note:* Night Mode chats get Automatically closed at 12 am(IST)
-and Automatically openned at 6 am(IST) To Prevent Night Spams.
+*Note*`:` Night Mode chats get Automatically closed at 12 AM (IST) and Automatically opened at 6 AM (IST) To Prevent Night Spamz.
 """
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
