222b16144831b8c1a83daf47c0b507ede44dad90
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 15:22:42 +0530

    Update cust_filters.py

--- MashaRoBot/modules/cust_filters.py
+++ MashaRoBot/modules/cust_filters.py
@@ -614,32 +614,34 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- ❍ /filters*:* List all active filters saved in the chat.
+• `/filters`*:* List all active filters saved in the chat.
 
 *Admin only:*
- ❍ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
+• `/filter` `<keyword>` `<reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. 
+NOTE: all filter \
+keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: `/filter "hey there" How you \
+doin?`
  Separate diff replies by `%%%` to get random replies
- *Example:* 
+ *Example* `:` 
  `/filter "filtername"
  Reply 1
  %%%
  Reply 2
  %%%
  Reply 3`
- ❍ /stop <filter keyword>*:* Stop that filter.
 
-*Chat creator only:*
- ❍ /removeallfilters*:* Remove all chat filters at once.
+• `/stop` `<filter keyword>`*:* Stop that filter.
 
-*Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check ❍ /markdownhelp to know more!
+*Chat creator only* `:`
+• `/removeallfilters`*:* Remove all chat filters at once.
+
+*Note*: Filters also support markdown formatters like: {first}, {last} etc. and buttons.
+Check • `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "FILTERS"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
