3cfc7b7bb757ea63804a107fba10896e6e50d84d
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 15:06:58 +0530

    Update tools.py

--- MashaRoBot/modules/tools.py
+++ MashaRoBot/modules/tools.py
@@ -2,19 +2,38 @@ __mod_name__ = "Tools"
 
 __help__ = """
 • `/country` `<country name>`*:* Gathering info about given *country*
-• `/cs`*:* Latest live scores from cricinfo
 • `/gps` `<location>`*:* Get gps location.
+
+• `/cs`*:* Latest live scores from cricinfo
  Example : `/gps India`
+
 • `/imdb` `<movie/seires name>`*:* Get full info about a movie & series with [imdb.com](https://m.imdb.com)
+
+*Zipper* :
 • `/zip`*:* reply to a telegram file to compress it in .zip format
 • `/unzip`*:* reply to a telegram file to decompress it from the .zip format
+
+*Weather* :
 • `/weather` `<city>`*:* Get weather info in a particular place.
 • `/wttr` `<city>`*:* Advanced weather module, usage same as /weather
  Eg. `/wttr moon`*:* Get the current status of moon
+
+*Secure* :
 • `/encrypt`*:* Encrypts The Given Text
 • `/decrypt`*:* Decrypts Previously Ecrypted Text
+
+*JSON* :
 • `/json`*:* Get JSON info about any message replied to.
+
+*Music* :
 • `/song` `<song name>`*:* Uploads the song in it's best quality available
  Ex: `/song Faded Alan Walker`
 • `/saavn` `<song name>`*:* Download song from saavn.
+
+*COVID* :
+• `/covid`*:* To Get Global Stats of Covid.
+• `/covid` `<COUNTRY>`*:* To Get Stats of A Single Country.
+"""
+
+__mod_name__ = "COVID"
 """
