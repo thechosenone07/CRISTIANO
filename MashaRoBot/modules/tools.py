fc6cd053c01df358cf86c237c58f1614f3a9f898
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 14:43:17 +0530

    Update tools.py

--- MashaRoBot/modules/tools.py
+++ MashaRoBot/modules/tools.py
@@ -1,12 +1,12 @@
 __mod_name__ = "Tools"
 
 __help__ = """
-
-
 • `/country` `<country name>`*:* Gathering info about given *country*
-
 • `/cs`*:* Latest live scores from cricinfo
-
 • `/gps` `<location>`*:* Get gps location.
  Example : `/gps India`
+• `/imdb` `<movie/seires name>`*:* Get full info about a movie & series with [imdb.com](https://m.imdb.com)
+• `/zip`*:* reply to a telegram file to compress it in .zip format
+• `/unzip`*:* reply to a telegram file to decompress it from the .zip format
+
 """
