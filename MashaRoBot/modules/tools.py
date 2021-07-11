09fb721ae2c5fea0ec8853b08ad44b927ec14df3
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 14:04:48 +0530

    Create tools.py

---
+++ MashaRoBot/modules/tools.py
@@ -0,0 +1,13 @@
+__help__ = """
+
+
+• `/country` `<country name>`*:* Gathering info about given *country*
+
+• `/cs`*:* Latest live scores from cricinfo
+
+• `/gps` `<location>`*:* Get gps location.
+ Example : `/gps India`
+
+"""
+
+__mod_name__ = "Tools"
