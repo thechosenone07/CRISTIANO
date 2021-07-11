9b73931d1617d23f3b553ff167d2451a42932d7b
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-07-11 12:18:52 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -71,6 +71,15 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1],))
 
+    # can only have a certain amount of buttons side by side
+
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("⏮️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Back", callback_data="home_back"),
+             EqInlineKeyboardButton("⏭️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+
+
     else:
         pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="masha_back")]]
 
