190e13124ea98939436cf47505a0016b0684ed0b
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 17:01:46 +0530

    Create heroku_help.py

---
+++ MashaRoBot/modules/heroku_help.py
@@ -0,0 +1,16 @@
+__mod_name__ = "Heroku"
+
+__help__ = """
+ ╔ *Senkuu Core:*
+ ╠ *Owner only*
+ ╠ `/send` `<module name>`*:* Send module
+ ╚ `/install` `<reply to a .py>`*:* Install module
+
+ ╔ *Heroku Settings:*
+ ╠ *Owner only*
+ ╠ `/usage`*:* Check your heroku dyno hours remaining.
+ ╠ `/see` `var `<var>`*:* Get your existing varibles, use it only on your private group
+ ╠ `/set` `var` `<newvar>` `<vavariable>`*:* Add new variable or update existing value variable
+ ╠ `/del` `var` `<var>`*:* Delete existing variable
+ ╚ `/logs`*:* Get heroku dyno logs
+"""
