b7684208139027cfc89033bce83f1ce57d37f5b8
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 10:29:47 +0300

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -85,7 +85,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
 
     else:
-        pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="masha_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙", callback_data="masha_back")]]
 
     return pairs
 
