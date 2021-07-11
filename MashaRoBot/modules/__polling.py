eabd3bda0123abec72549b2223fb617513d79c5c
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 14:13:31 +0530

    Update __polling.py

--- MashaRoBot/modules/__polling.py
+++ MashaRoBot/modules/__polling.py
@@ -394,24 +394,28 @@ async def stop(event):
 
 
 __help__ = """
-You can now send polls anonymously with Masha
+You can now send polls anonymously with Senkuu.
 Here is how you can do it:
-**Parameters** -
- ❍ poll-id - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
- ❍ question - the question you wanna ask
- ❍ <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
- ❍ <True/False>(2) - public votes
- ❍ <True/False>(3) - multiple choice
-**Syntax** -
-❍ `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
-**Examples** -
-❍ `/poll 12345 | am i cool? | False False False yes no`
-❍ `/poll 12345 | am i cool? | True@1 False False yes no`
-**To stop a poll**
-Reply to the poll with `/stoppoll <poll-id>` to stop the poll
-**NOTE**
+
+*Parameters* -
+• `poll-id` - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
+• `question` - the question you wanna ask
+• `<True@optionnumber/False>(1)` - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
+• `<True/False>(2)` - public votes
+• `<True/False>(3)` - multiple choice
+
+*Syntax* -
+• `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
+
+*Examples* -
+• `/poll 12345 | am i cool? | False False False yes no`
+• `/poll 12345 | am i cool? | True@1 False False yes no`
+
+*To stop a poll* -
+Reply to the poll with `/stoppoll` OR send `/stoppoll <poll-id>` in chat.
+
+*NOTE* -
 If you have forgotten your poll id or deleted the poll so that you can't stop the previous poll type `/forgotpollid`, this will reset the poll id, you will have no access to the previous poll !
 """
 
-
-__mod_name__ = "POLLING"
+__mod_name__ = "Polling"
