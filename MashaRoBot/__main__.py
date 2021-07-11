bf2d5c6fe24f82092e6869d92b1e8828c553fc4a
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 10:40:11 +0300

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -75,7 +75,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hey there, I'm *Senkuu*!
-I'm here to help you manage your groups! Hit Help button below to find out more about how to use me to my full potential[.](https://images6.alphacoders.com/102/thumb-1920-1027254.jpg)
+I'm here to help you manage your groups! Hit Help button below to find out more about how to use me to my full potential[.](https://images6.alphacoders.co/102/thumb-1920-1027254.jpg)
 """
 
 buttons = [
