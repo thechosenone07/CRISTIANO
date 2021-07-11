4dcee2b5bce887d117214af4aaba4ad9d217ac16
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-11 17:05:59 +0530

    Update heroku_help.py

--- MashaRoBot/modules/heroku_help.py
+++ MashaRoBot/modules/heroku_help.py
@@ -1,6 +1,7 @@
 __mod_name__ = "Heroku"
 
 __help__ = """
+
  ╔ *Senkuu Core:*
  ╠ *Owner only*
  ╠ `/send` `<module name>`*:* Send module
@@ -9,8 +10,8 @@ __help__ = """
  ╔ *Heroku Settings:*
  ╠ *Owner only*
  ╠ `/usage`*:* Check your heroku dyno hours remaining.
- ╠ `/see` `var `<var>`*:* Get your existing varibles, use it only on your private group
- ╠ `/set` `var` `<newvar>` `<vavariable>`*:* Add new variable or update existing value variable
- ╠ `/del` `var` `<var>`*:* Delete existing variable
+ ╠ `/see` `var <var>`*:* Get your existing varibles, use it only on your private group
+ ╠ `/set` `var <newvar>` `<vavariable>`*:* Add new variable or update existing value variable
+ ╠ `/del` `var <var>`*:* Delete existing variable
  ╚ `/logs`*:* Get heroku dyno logs
 """
