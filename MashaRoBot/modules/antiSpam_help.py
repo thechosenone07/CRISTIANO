040a776aad95c298dc159cbbd3bf7459db87315a
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-07-12 12:41:41 +0530

    Create antiSpam_help.py

---
+++ MashaRoBot/modules/antiSpam_help.py
@@ -0,0 +1,18 @@
+__mod_name__ = "Anti-Spam"
+
+__help__ = """
+*Admins only:*
+• `/antispam` `<on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+
+*Anti-Spam*, used by bot devs to ban spammers across all groups. This helps *protect* you and your groups by removing spam flooders as quickly as possible.
+
+*Note:* Users can *appeal* gbans or *report* spammers at @SenkuSupport.
+
+This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
+
+*What is SpamWatch*?
+*SpamWatch* maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters.
+Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
+
+*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
+"""
